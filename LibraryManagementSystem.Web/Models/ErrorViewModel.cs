2b540012eac0f47d763f6a07d72720f35f2f3392
Yehia Mohamed <Yehia Mohamed@DESKTOP-CREEPCF>
2025-06-02 21:00:33 +0300

    Add project files.

---
+++ LibraryManagementSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagementSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
